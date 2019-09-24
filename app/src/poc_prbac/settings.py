d800c4e5bf09d74afdd9d336f110d302146b9eb1
titanlin <csiejer@gmail.com>
2019-09-25 02:50:59 +0800

    fix permission for django static and media

--- app/src/poc_prbac/settings.py
+++ app/src/poc_prbac/settings.py
@@ -122,5 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static'
-MEDIA_ROOT = '/media'
+STATIC_ROOT = '/data/static'
+MEDIA_ROOT = '/data/media'
