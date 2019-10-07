80c78d3d3c8ebb23862bf354b5da75310de72250
Titan Lin <csiejer@gmail.com>
2019-10-08 05:50:06 +0800

    add comments for test

--- app/src/test_example_gogolook.py
+++ app/src/test_example_gogolook.py
@@ -15,6 +15,7 @@ userreport_write = Role.objects.get(slug='record') \
                                .instantiate({'record': 'userreport',
                                              'action': 'write'})
 
+# check record permission for user
 assert Role.objects.get(slug='u_2').has_privilege(userreport_read) is True
 assert Role.objects.get(slug='u_2').has_privilege(userreport_write) is True
 
@@ -25,6 +26,7 @@ assert Role.objects.get(slug='u_4').has_privilege(userreport_read) is True
 assert Role.objects.get(slug='u_4').has_privilege(userreport_write) is False
 
 
+# check group admin permission for user
 group_infra = Role.objects.get(slug='g_infra')
 group_fintech = Role.objects.get(slug='g_fintech')
 
@@ -32,3 +34,5 @@ assert Role.objects.get(slug='a_2').has_privilege(group_infra) is True
 assert Role.objects.get(slug='a_2').has_privilege(group_fintech) is True
 assert Role.objects.get(slug='a_3').has_privilege(group_fintech) is False
 assert Role.objects.get(slug='a_4').has_privilege(group_fintech) is False
+
+print('Test Successfully!')
